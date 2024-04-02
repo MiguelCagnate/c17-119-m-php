e77dfe93e1b7aa14d2172b279126ed783fd499ce
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-04-02 18:46:05 -0500

    public

--- pet-client/src/main.jsx
+++ pet-client/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import Router from './router.jsx'
